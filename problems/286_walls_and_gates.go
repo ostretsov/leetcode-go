ace9f2e6cfd1a819e9fe47acdab9d6b8651ec887
Artem Ostretsov <OstretsovAA@gmail.com>
2024-10-06 23:06:39 -0300

    upd walls and gates: fast & mem efficient

--- problems/286_walls_and_gates.go
+++ problems/286_walls_and_gates.go
@@ -7,7 +7,7 @@ import (
 
 // https://leetcode.com/problems/walls-and-gates/description/
 func wallsAndGates(rooms [][]int) {
-	wallsAndGatesV1Slow(rooms)
+	wallsAndGatesV3MemEfficient(rooms)
 }
 
 func wallsAndGatesV1Slow(rooms [][]int) {
@@ -63,6 +63,117 @@ func wallsAndGatesV1Slow(rooms [][]int) {
 			visitNotVisitedRoom(visitedRooms, c.m, c.n-1, c.dstFromGt+1)
 			visitNotVisitedRoom(visitedRooms, c.m+1, c.n, c.dstFromGt+1)
 			visitNotVisitedRoom(visitedRooms, c.m-1, c.n, c.dstFromGt+1)
+		}
+	}
+}
+
+// Memory Limit Exceeded
+func wallsAndGatesV2Faster(rooms [][]int) {
+	mLen := len(rooms)
+	nLen := len(rooms[0])
+
+	gSearchM, gSearchN := 0, 0
+	nextGates := func() (int, int, bool) {
+		for ; gSearchM < mLen; gSearchM++ {
+			for ; gSearchN < nLen; gSearchN++ {
+				if rooms[gSearchM][gSearchN] == 0 {
+					defer func() { gSearchN++ }()
+					return gSearchM, gSearchN, true
+				}
+			}
+			gSearchN = 0
+		}
+		return 0, 0, false
+	}
+
+	type coords struct {
+		m, n      int
+		dstFromGt int
+	}
+
+	queue := kit.SimpleQueue[coords]{}
+	visitNotVisitedRoom := func(visitedRooms [][]bool, m, n, dstFromGt int) {
+		if m < 0 || m >= mLen || n < 0 || n >= nLen {
+			return
+		}
+		if rooms[m][n] == -1 || rooms[m][n] == 0 || visitedRooms[m][n] {
+			return
+		}
+		queue.Enqueue(coords{m, n, dstFromGt})
+	}
+
+	for {
+		mGt, nGt, found := nextGates()
+		if !found {
+			break
+		}
+
+		visitedRooms := utils.ZeroSliceMxN[bool](mLen, nLen)
+		queue.Enqueue(coords{mGt, nGt, 0})
+		for !queue.Empty() {
+			c, _ := queue.Dequeue()
+			if c.dstFromGt < rooms[c.m][c.n] {
+				rooms[c.m][c.n] = c.dstFromGt
+			}
+			visitedRooms[c.m][c.n] = true
+
+			visitNotVisitedRoom(visitedRooms, c.m, c.n+1, c.dstFromGt+1)
+			visitNotVisitedRoom(visitedRooms, c.m, c.n-1, c.dstFromGt+1)
+			visitNotVisitedRoom(visitedRooms, c.m+1, c.n, c.dstFromGt+1)
+			visitNotVisitedRoom(visitedRooms, c.m-1, c.n, c.dstFromGt+1)
+		}
+	}
+}
+
+func wallsAndGatesV3MemEfficient(rooms [][]int) {
+	mLen := len(rooms)
+	nLen := len(rooms[0])
+
+	gSearchM, gSearchN := 0, 0
+	nextGates := func() (int, int, bool) {
+		for ; gSearchM < mLen; gSearchM++ {
+			for ; gSearchN < nLen; gSearchN++ {
+				if rooms[gSearchM][gSearchN] == 0 {
+					defer func() { gSearchN++ }()
+					return gSearchM, gSearchN, true
+				}
+			}
+			gSearchN = 0
+		}
+		return 0, 0, false
+	}
+
+	type coords struct {
+		m, n      int
+		dstFromGt int
+	}
+
+	for {
+		mGt, nGt, found := nextGates()
+		if !found {
+			break
+		}
+
+		queue := kit.SimpleQueue[coords]{}
+		visitNotVisitedRoom := func(m, n, dstFromGt int) {
+			if m < 0 || m >= mLen || n < 0 || n >= nLen {
+				return
+			}
+			if rooms[m][n] == -1 || rooms[m][n] == 0 || rooms[m][n] <= dstFromGt {
+				return
+			}
+			rooms[m][n] = dstFromGt
+			queue.Enqueue(coords{m, n, dstFromGt})
+		}
+
+		queue.Enqueue(coords{mGt, nGt, 0})
+		for !queue.Empty() {
+			c, _ := queue.Dequeue()
+
+			visitNotVisitedRoom(c.m, c.n+1, c.dstFromGt+1)
+			visitNotVisitedRoom(c.m, c.n-1, c.dstFromGt+1)
+			visitNotVisitedRoom(c.m+1, c.n, c.dstFromGt+1)
+			visitNotVisitedRoom(c.m-1, c.n, c.dstFromGt+1)
 		}
 	}
 }
--- problems/286_walls_and_gates_test.go
+++ problems/286_walls_and_gates_test.go
@@ -2,17 +2,23 @@ package problems
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_wallsAndGates(t *testing.T) {
-	const e = 2147483647
+	const (
+		r = 2147483647
+		G = 0
+		W = -1
+	)
+
 	map1 := [][]int{
-		{e, -1, 0, e},
-		{e, e, e, -1},
-		{e, -1, e, -1},
-		{0, -1, e, e},
+		{r, W, G, r},
+		{r, r, r, W},
+		{r, W, r, W},
+		{G, W, r, r},
 	}
 	wallsAndGates(map1)
 	assert.Equal(t, []int{3, -1, 0, 1}, map1[0])
@@ -20,26 +26,53 @@ func Test_wallsAndGates(t *testing.T) {
 	assert.Equal(t, []int{1, -1, 2, -1}, map1[2])
 	assert.Equal(t, []int{0, -1, 3, 4}, map1[3])
 
-	//map2 := [][]int{
-	//	{0, e, -1, e, -1, -1, 0, -1, e, e, e, 0, 0, e, -1, 0, e, -1, e, e, 0, 0, -1, 0, e, -1, e, -1, -1, -1, e, e, 0, e, e, -1, 0, 0, 0, 0, e, e, 0, e, e, 0},
-	//	{-1, 0, e, -1, -1, e, e, e, 0, 0, 0, e, 0, -1, 0, -1, 0, -1, e, 0, -1, -1, e, -1, e, 0, e, e, -1, -1, e, 0, e, e, 0, 0, 0, -1, e, -1, -1, e, 0, -1, 0, 0},
-	//	{-1, 0, 0, e, 0, e, 0, e, 0, 0, 0, 0, -1, e, e, -1, e, -1, e, -1, e, -1, e, -1, e, 0, 0, e, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, -1, e, e, -1, -1, e, 0},
-	//	{0, -1, e, e, 0, e, 0, e, -1, e, -1, e, e, e, e, -1, 0, e, 0, -1, -1, -1, 0, 0, 0, -1, e, 0, -1, e, e, -1, -1, 0, -1, e, 0, e, 0, 0, e, -1, -1, e, e, 0},
-	//	{-1, -1, 0, 0, e, e, 0, 0, e, -1, 0, -1, 0, 0, e, 0, e, 0, e, 0, e, e, 0, e, -1, 0, 0, e, 0, e, e, e, -1, e, -1, e, 0, 0, -1, e, -1, 0, -1, e, -1, e},
-	//	{0, -1, e, -1, 0, -1, -1, e, e, 0, e, 0, -1, 0, e, -1, e, e, 0, -1, 0, 0, e, -1, 0, -1, e, -1, e, 0, 0, 0, 0, e, -1, 0, e, e, -1, e, 0, -1, e, 0, e, e},
-	//	{0, 0, 0, e, -1, 0, e, -1, e, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, e, -1, 0, e, 0, e, -1, 0, -1, -1, e, 0, e, -1, 0, 0, -1, 0, -1, e, -1, e, e, -1, 0, 0, 0},
-	//	{e, e, e, 0, 0, e, 0, -1, 0, -1, -1, -1, 0, e, -1, -1, -1, e, -1, e, -1, 0, e, -1, e, 0, e, -1, -1, e, -1, 0, 0, e, -1, e, 0, 0, 0, e, 0, -1, -1, 0, 0, -1},
-	//	{-1, 0, -1, e, e, 0, e, e, -1, 0, -1, e, 0, 0, -1, 0, -1, -1, 0, 0, -1, -1, e, e, e, 0, e, -1, -1, 0, e, -1, 0, e, -1, -1, 0, e, -1, e, e, 0, e, -1, e, e},
-	//	{-1, e, -1, e, -1, e, -1, -1, -1, 0, -1, 0, e, 0, 0, 0, -1, -1, -1, 0, 0, -1, -1, -1, 0, -1, e, e, -1, 0, e, e, e, 0, e, -1, e, 0, -1, -1, e, -1, e, 0, e, -1},
-	//	{0, 0, e, 0, e, e, 0, -1, -1, -1, 0, -1, -1, -1, 0, 0, -1, e, e, 0, 0, 0, -1, 0, -1, -1, 0, -1, -1, 0, 0, 0, 0, e, -1, e, e, -1, e, -1, e, -1, 0, 0, -1, -1},
-	//	{-1, e, e, 0, 0, e, -1, e, 0, e, 0, 0, e, e, 0, -1, 0, -1, e, e, -1, e, -1, -1, e, e, 0, -1, 0, e, 0, 0, e, e, -1, e, 0, -1, 0, 0, -1, 0, 0, 0, -1, -1},
-	//	{0, -1, e, 0, -1, -1, 0, -1, e, -1, -1, -1, -1, e, -1, e, e, e, e, -1, -1, e, -1, e, -1, e, -1, e, -1, e, 0, e, 0, -1, -1, 0, 0, -1, 0, e, -1, -1, 0, e, 0, 0},
-	//	{-1, -1, 0, -1, e, -1, 0, -1, e, e, e, -1, 0, -1, -1, 0, 0, 0, 0, 0, 0, e, 0, -1, e, 0, 0, -1, e, e, e, e, -1, 0, 0, 0, 0, -1, e, 0, 0, 0, 0, -1, e, -1},
-	//	{e, 0, 0, e, 0, e, -1, 0, 0, e, e, e, e, e, 0, -1, 0, -1, -1, e, -1, 0, -1, -1, 0, e, e, -1, -1, e, e, -1, e, e, -1, e, 0, 0, 0, -1, -1, 0, 0, 0, 0, e},
-	//	{0, -1, -1, 0, 0, -1, -1, -1, 0, 0, e, e, e, e, -1, -1, 0, -1, -1, -1, 0, e, e, 0, e, 0, 0, 0, 0, e, 0, e, e, 0, -1, e, -1, -1, e, e, e, -1, e, 0, -1, e},
-	//	{-1, -1, -1, -1, e, 0, 0, e, 0, 0, e, 0, -1, 0, e, 0, 0, e, 0, -1, 0, 0, 0, 0, 0, -1, 0, e, e, 0, 0, e, 0, 0, e, e, -1, e, e, e, 0, -1, -1, 0, e, e},
-	//	{e, -1, -1, 0, e, -1, 0, 0, -1, -1, e, 0, 0, -1, 0, e, e, -1, e, -1, 0, e, -1, -1, 0, e, -1, 0, -1, -1, -1, -1, e, -1, -1, 0, -1, e, -1, e, -1, e, 0, -1, 0, 0},
-	//	{e, e, -1, e, -1, 0, 0, 0, e, e, 0, -1, -1, 0, -1, e, e, -1, 0, e, e, e, 0, 0, e, e, 0, e, -1, e, e, 0, e, 0, e, e, e, 0, -1, e, e, e, e, -1, -1, e},
-	//}
-	//wallsAndGates(map2)
+	map2 := [][]int{
+		{G, r, W, r, W, W, G, W, r, r, r, G, G, r, W, G, r, W, r, r, G, G, W, G, r, W, r, W, W, W, r, r, G, r, r, W, G, G, G, G, r, r, G, r, r, G},
+		{W, G, r, W, W, r, r, r, G, G, G, r, G, W, G, W, G, W, r, G, W, W, r, W, r, G, r, r, W, W, r, G, r, r, G, G, G, W, r, W, W, r, G, W, G, G},
+		{W, G, G, r, G, r, G, r, G, G, G, G, W, r, r, W, r, W, r, W, r, W, r, W, r, G, G, r, W, G, W, G, G, G, G, W, G, W, G, W, r, r, W, W, r, G},
+		{G, W, r, r, G, r, G, r, W, r, W, r, r, r, r, W, G, r, G, W, W, W, G, G, G, W, r, G, W, r, r, W, W, G, W, r, G, r, G, G, r, W, W, r, r, G},
+		{W, W, G, G, r, r, G, G, r, W, G, W, G, G, r, G, r, G, r, G, r, r, G, r, W, G, G, r, G, r, r, r, W, r, W, r, G, G, W, r, W, G, W, r, W, r},
+		{G, W, r, W, G, W, W, r, r, G, r, G, W, G, r, W, r, r, G, W, G, G, r, W, G, W, r, W, r, G, G, G, G, r, W, G, r, r, W, r, G, W, r, G, r, r},
+		{G, G, G, r, W, G, r, W, r, W, W, W, W, W, W, W, W, G, G, r, W, G, r, G, r, W, G, W, W, r, G, r, W, G, G, W, G, W, r, W, r, r, W, G, G, G},
+		{r, r, r, G, G, r, G, W, G, W, W, W, G, r, W, W, W, r, W, r, W, G, r, W, r, G, r, W, W, r, W, G, G, r, W, r, G, G, G, r, G, W, W, G, G, W},
+		{W, G, W, r, r, G, r, r, W, G, W, r, G, G, W, G, W, W, G, G, W, W, r, r, r, G, r, W, W, G, r, W, G, r, W, W, G, r, W, r, r, G, r, W, r, r},
+		{W, r, W, r, W, r, W, W, W, G, W, G, r, G, G, G, W, W, W, G, G, W, W, W, G, W, r, r, W, G, r, r, r, G, r, W, r, G, W, W, r, W, r, G, r, W},
+		{G, G, r, G, r, r, G, W, W, W, G, W, W, W, G, G, W, r, r, G, G, G, W, G, W, W, G, W, W, G, G, G, G, r, W, r, r, W, r, W, r, W, G, G, W, W},
+		{W, r, r, G, G, r, W, r, G, r, G, G, r, r, G, W, G, W, r, r, W, r, W, W, r, r, G, W, G, r, G, G, r, r, W, r, G, W, G, G, W, G, G, G, W, W},
+		{G, W, r, G, W, W, G, W, r, W, W, W, W, r, W, r, r, r, r, W, W, r, W, r, W, r, W, r, W, r, G, r, G, W, W, G, G, W, G, r, W, W, G, r, G, G},
+		{W, W, G, W, r, W, G, W, r, r, r, W, G, W, W, G, G, G, G, G, G, r, G, W, r, G, G, W, r, r, r, r, W, G, G, G, G, W, r, G, G, G, G, W, r, W},
+		{r, G, G, r, G, r, W, G, G, r, r, r, r, r, G, W, G, W, W, r, W, G, W, W, G, r, r, W, W, r, r, W, r, r, W, r, G, G, G, W, W, G, G, G, G, r},
+		{G, W, W, G, G, W, W, W, G, G, r, r, r, r, W, W, G, W, W, W, G, r, r, G, r, G, G, G, G, r, G, r, r, G, W, r, W, W, r, r, r, W, r, G, W, r},
+		{W, W, W, W, r, G, G, r, G, G, r, G, W, G, r, G, G, r, G, W, G, G, G, G, G, W, G, r, r, G, G, r, G, G, r, r, W, r, r, r, G, W, W, G, r, r},
+		{r, W, W, G, r, W, G, G, W, W, r, G, G, W, G, r, r, W, r, W, G, r, W, W, G, r, W, G, W, W, W, W, r, W, W, G, W, r, W, r, W, r, G, W, G, G},
+		{r, r, W, r, W, G, G, G, r, r, G, W, W, G, W, r, r, W, G, r, r, r, G, G, r, r, G, r, W, r, r, G, r, G, r, r, r, G, W, r, r, r, r, W, W, r},
+	}
+	start := time.Now()
+	wallsAndGates(map2)
+	execTime := time.Since(start)
+	assert.Less(t, execTime, 1*time.Second)
+
+	map3 := [][]int{
+		{G, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r},
+		{r, G, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r},
+		{r, r, G, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r},
+		{r, r, r, G, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r},
+		{r, r, r, r, G, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r},
+		{r, r, r, r, r, G, r, r, r, r, r, r, r, r, r, r, r, r, r, r},
+		{r, r, r, r, r, r, G, r, r, r, r, r, r, r, r, r, r, r, r, r},
+		{r, r, r, r, r, r, r, G, r, r, r, r, r, r, r, r, r, r, r, r},
+		{r, r, r, r, r, r, r, r, G, r, r, r, r, r, r, r, r, r, r, r},
+		{r, r, r, r, r, r, r, r, r, G, r, r, r, r, r, r, r, r, r, r},
+		{r, r, r, r, r, r, r, r, r, r, G, r, r, r, r, r, r, r, r, r},
+		{r, r, r, r, r, r, r, r, r, r, r, G, r, r, r, r, r, r, r, r},
+		{r, r, r, r, r, r, r, r, r, r, r, r, G, r, r, r, r, r, r, r},
+		{r, r, r, r, r, r, r, r, r, r, r, r, r, G, r, r, r, r, r, r},
+		{r, r, r, r, r, r, r, r, r, r, r, r, r, r, G, r, r, r, r, r},
+		{r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, G, r, r, r, r},
+		{r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, G, r, r, r},
+		{r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, G, r, r},
+		{r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, G, r},
+		{r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, r, G},
+	}
+	wallsAndGates(map3)
 }
